afa96857f29d515cc63c6dc8cbfcdbb1a1c4db43
andy-laptop <daubner.andy@gmail.com>
2021-10-30 16:59:28 +0200

    added index and delta changes

--- free_games_collection.py
+++ free_games_collection.py
@@ -1,17 +1,21 @@
 import sqlite3
 from sqlite3 import Error
+import difflib as diff
 from datetime import datetime, timezone
 import epic_api_fetch
 from epicstore_api import EpicGamesStoreAPI, OfferData
 
-DB_PATH = r'C:\Users\Andy\Documents\_python\epic-games-bot\free_games.db'#TODO fix path, change to relative.
+# DB_PATH = r'C:\Users\Andy\Documents\_python\epic-games-bot\free_games.db'#_TODO fix path, change to relative.
+DB_PATH_LINUX = r'/home/andy/PycharmProjects/epic-games-notification-bot/free_games.db'
 API=EpicGamesStoreAPI()
+DB_NAME = 'games'
 """
 class connectionError(Exception):
 	#https://stackabuse.com/how-to-print-colored-text-in-python/
 	print("\n\033[1;31;43m Please uncomment the connection (ctrl+f \"con\"), this exception is here to prevent accidental spamming of database. \033[0;0m\n")
 raise connectionError
 """
+
 
 def insert_data(data):
 	"""Inserts one entry into games table of free_games.db
@@ -20,36 +24,45 @@ def insert_data(data):
 	Set notes to 'test' when making entry that should be deleted later
 	"""
 	#reference for sql https://www.w3schools.com/sql/sql_insert.asp
-	cur.execute(f"INSERT INTO games VALUES (\"{data[0]}\",\"{data[1]}\",\"{data[2]}\",\"{data[3]}\",\"{data[4]}\",\"{data[5]}\")")#no semi colon needed, double vs single quotes does not matter
+	cur.execute(f"INSERT INTO {DB_NAME} VALUES ({data[0]},\"{data[1]}\",\"{data[2]}\",\"{data[3]}\",\"{data[4]}\",\"{data[5]}\",\"{data[6]}\")")#no semi colon needed, double vs single quotes does not matter
 	con.commit()
 
-def get_last_entry():
-	last_id = cur.lastrowid
-	last = cur.execute(f"SELECT * FROM games WHERE rowid = {last_id}")  #_FIXME do NOT select * pass the column as a parameter
+
+def get_last_entry(column):
+	"""Make request to database for
+
+	"""
+	cur.execute(f'SELECT {column} FROM {DB_NAME} ORDER BY id DESC LIMIT 1')  #Get last entry by id column
+	last = cur.fetchall()[0][0]  #[0][0] reqired to remove list and tuple onion
 	return last
 
-def calculate_change(current):
+
+def get_last_index():
+	cur.execute(f'SELECT id From {DB_NAME} ORDER BY id DESC LIMIT 1')
+	return int(cur.fetchall()[0][0])  #[0][0] reqired to remove list and tuple onion
+
+
+def calculate_change(current, column):
 	"""
-	Precon: len(current) >= len(last)
+	Docs blablabla
 	"""
-	last = get_last_entry()
-	difference = ''
-	for i in range(len(current)):
-		if current[i] != last[i]:
-			difference += current[i]
+	last = get_last_entry(column)
+	difference_gen = diff.ndiff(last, current)
+	difference = ''.join(difference_gen)
 	return difference
+
 
 def main():
 	global con, cur
-	con = sqlite3.connect(DB_PATH)
+	con = sqlite3.connect(DB_PATH_LINUX)
 	cur = con.cursor()
 	#gathering variables
 	time_stamp = datetime.now(timezone.utc).isoformat()
-	api_return = str(API.get_free_games()).replace('"', "'")  #_TODO calculate api_return change
-	api_return_change = calculate_change(api_return)
-	free_games = epic_api_fetch.get_games()
-	free_games_change = calculate_change(free_games)
-	insert_data((time_stamp, api_return, api_return_change, free_games, free_games_change, 'test'))
+	api_return = str(API.get_free_games()).replace('"', "'")
+	api_return_change = calculate_change(api_return, 'api_return')
+	free_games = str(epic_api_fetch.get_games())
+	free_games_change = calculate_change(free_games, 'free_games')
+	insert_data((get_last_index() + 1, time_stamp, api_return, api_return_change, free_games, free_games_change, 'test'))
 	con.close()
 
 
