540bc33463dcf5389cf20c2a7b87f90e933e7dd5
AX-11 <daubner.andy@gmail.com>
2023-04-20 17:19:56 +0200

    new start, complete rewrite

--- bot.py
+++ bot.py
@@ -1,57 +1,41 @@
-import epic_api_fetch#my own code, might break
 import discord
-import json
-#####################################################################
-#Andy Daubner (daubner.andy@gmail.com)
-#24/07/21
-#
-#Async app to run python bot that fetches data from epic games.
-#!!!DON'T UPLOAD config.json TO GITHUB!!!
-#
-#####################################################################
-
-#global variables, will require restart if changed
-config=json.load(open('config.json'))
-TOKEN=config['discord_token']
-CMD_TAG=config['command_prefix']
-CHNL_WHITELIST=config['whitelisted_channels']
+from discord.ext import tasks
+import my_secrets
 
 
-client=discord.Client()
+
+
+# Global variables
+CHECKING_INTERVAL = 10 # time in seconds between epic games checking
+
+
+intents = discord.Intents.default()
+client = discord.Client(intents=intents)
 
 #confirmation bot is online
 @client.event
 async def on_ready():
-	print('logged in as '+str(client.user))
-
-
+    print(f"logged in as {str(client.user)}")
+    await check_epic_games.start()
+    # TODO Why does code not run here? vv
+    print(f"epic games checker started with polling rate of {CHECKING_INTERVAL}sec")
+    
 @client.event
-async def on_message(message):
-	#variables
-	channel=message.channel#use str() to read channel
-	author=message.author
-	content=message.content
-	#self recursion check
-	if author == client.user:
-		return
-	#whitelisted channel
-	if str(channel) not in CHNL_WHITELIST:
-		return
-	#TODO Main logic
-	if content.startswith(CMD_TAG):
-		command=content[len(CMD_TAG):]#stores everything but the command tag
-		#help
-		if command.startswith('help'):
-			await channel.send('this is the temporary help page')
-		#get free games
-		if command.startswith('games'):
-			games = epic_api_fetch.get_games()
-			await channel.send(str(games))
-		#TODO add more commands
+async def on_message(message): #TODO use discord command system
+    #variables
+    channel=message.channel#use str() to read channel
+    author=message.author
+    content=message.content
+    #self recursion check
+    if author == client.user:
+        return
+    await channel.send("hello")
+
+@tasks.loop(seconds=CHECKING_INTERVAL)
+async def check_epic_games():
+    print("hello")
 
 
 
-#############runtime###############
-
-if __name__=="__main__":
-	client.run(TOKEN)
+if __name__ == "__main__":
+    client.run(my_secrets.discord_api_key)
