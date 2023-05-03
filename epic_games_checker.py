e25a77ef401e32790fccd50ef439759d2032bcc4
AX-11 <daubner.andy@gmail.com>
2023-05-03 21:53:19 +0200

    parser functional, finds currently free games

--- epic_games_checker.py
+++ epic_games_checker.py
@@ -1,4 +1,6 @@
 import epicstore_api
+from logging import warning
+from datetime import datetime
 
 api = epicstore_api.EpicGamesStoreAPI()
 
@@ -16,19 +18,48 @@ def get_free_games():
     return games
 
 
-games = [
-    {
-        "title":"asdf",
-        "description":"asdf"
-    }
-]
 
 # TODO when to notify, twice per game? notify about upcoming free games? Ideally you want it once, right as it becomes free, and on request if later. Two channels?
 # TODO log when data does not fit expected model
+# TODO return current, and upcoming promotions?
+# TODO model api return
 # parse api return to common object type
 # will only return TRUE free games
 def parse_games(api_return):
-    return
+    games = list()
+    try:
+        api_return = api_return["data"]["Catalog"]["searchStore"]["elements"]
+    except KeyError:
+        warning("Could not parse epic games api_return: " + api_return)
+    for game in api_return:
+        # Check if game is valid free game
+        # Game cannot be DLC
+        if(game["offerType"] == "DLC"):
+            continue
+        
+        # Game must cost 0 eur/usd/...
+        if(game["price"]["totalPrice"]["discountPrice"] != 0):
+            continue
+        
+        # Game must have a currently active promotion
+        if(len(game["promotions"]["promotionalOffers"]) == 0):
+            continue
+        
+        # TODO add more conditions
+        
+        # At this point we know that the game is currently free
+        # Generate a universal game object
+        free_until = datetime.fromisoformat(game["promotions"]["promotionalOffers"][0]["promotionalOffers"][0]["endDate"])
+        free_game = {
+            "title": game["title"],
+            "description":game["description"],
+            "free_until":free_until.strftime("%d/%m/%Y")
+        }
+        games.append(free_game)
+    return games
+        
+        
+        
 
 
 # maybe?
@@ -46,4 +77,5 @@ def check_games():
 
 # TODO removeme
 if __name__ == '__main__':
-    get_free_games()
+    games = get_free_games()
+    pass
