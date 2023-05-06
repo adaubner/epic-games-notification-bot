791dcc3b43e69168120916fdd61cc1d285ba31d1
AX-11 <daubner.andy@gmail.com>
2023-05-06 22:10:29 +0200

    version 1.0

--- discord_bot.py
+++ discord_bot.py
@@ -1,41 +1,81 @@
 import discord
+from discord import app_commands
 from discord.ext import tasks
-import my_secrets
+
+from dotenv import load_dotenv
+from os import getenv
+import json
+
 from epic_games_checker import check_games
 
 
-
 # Global variables
-CHECKING_INTERVAL = 10 # time in seconds between epic games checking
+CHECKING_INTERVAL = 10  # time in seconds between epic games checking
 
 
 intents = discord.Intents.default()
 client = discord.Client(intents=intents)
+tree = app_commands.CommandTree(client)
 
-#confirmation bot is online
+# Key: guild id
+# Value: list of channels
+# whitelist_channels = {}  # TODO sql
+
+
+@tree.command(name="help")
+async def help(interaction):
+    await interaction.response.send_message("help page here")
+
+
+# TODO remove channel command
+@tree.command(
+    name="add_channel",
+    description="Notifies this channel about new games"
+)
+async def add_channel(interacion):
+    await interacion.response.send_message(f'Will notify channel "{interacion.channel}" about new games.')
+    guild_id = str(interacion.guild_id)
+    channel_id = interacion.channel_id
+    with open(r"whitelist_channels.json") as f:
+        whitelist_channels = json.load(f)
+    # Does the guild currently exist in the whitelist
+    
+    if guild_id not in whitelist_channels:
+        whitelist_channels[guild_id] = list()
+    # If channel is new, add it
+    if channel_id in whitelist_channels[guild_id]:
+        return
+    whitelist_channels[guild_id].append(channel_id)
+    with open(r"whitelist_channels.json", "w") as f:
+        json.dump(whitelist_channels, f)
+    
+
+
+# confirmation bot is online
 @client.event
 async def on_ready():
     print(f"logged in as {str(client.user)}")
+    await tree.sync()
     await check_epic_games.start()
-    
-@client.event
-async def on_message(message): #TODO use discord command system
-    #variables
-    channel=message.channel#use str() to read channel
-    author=message.author
-    content=message.content
-    #self recursion check
-    if author == client.user:
-        return
-    await channel.send("hello")
+
 
 @tasks.loop(seconds=CHECKING_INTERVAL)
 async def check_epic_games():
     new_free_games = check_games()
-    if new_free_games is not None:
-        print("new games discord message")
-
+    if len(new_free_games) != 0:
+        print(new_free_games)  # TODO debug
+        
+        with open(r"whitelist_channels.json") as f:
+            whitelist_channels = json.load(f)
+        
+        for guild_id, channels in whitelist_channels.items():
+            for channel_id in channels:
+                guild_id = int(guild_id)
+                channel = client.get_guild(guild_id).get_channel(channel_id)
+                await channel.send("New free games:\n" + str(new_free_games))
 
 
 if __name__ == "__main__":
-    client.run(my_secrets.discord_api_key)
+    load_dotenv()
+    discord_token = getenv("DISCORD_TOKEN")
+    client.run(discord_token)
--- epic_games_checker.py
+++ epic_games_checker.py
@@ -5,18 +5,12 @@ from logging import warning
 api = epicstore_api.EpicGamesStoreAPI()
 
 
-
-
-
 # make a call to the epic store api and return parsed free games
+# TODO is this needed?
 def get_free_games():
     api_return = api.get_free_games()
-    import json
-    with open('temp.json', 'w') as f:
-        f.write(json.dumps(api_return))
     games = parse_games(api_return)
     return games
-
 
 
 # TODO when to notify, twice per game? notify about upcoming free games? Ideally you want it once, right as it becomes free, and on request if later. Two channels?
@@ -34,65 +28,69 @@ def parse_games(api_return):
     for game in api_return:
         # Check if game is valid free game
         # Game cannot be DLC
-        if(game["offerType"] == "DLC"):
+        if game["offerType"] == "DLC":
             continue
-        
+
         # Game must cost 0 eur/usd/...
-        if(game["price"]["totalPrice"]["discountPrice"] != 0):
+        if game["price"]["totalPrice"]["discountPrice"] != 0:
             continue
-        
+
         # Game must have a currently active promotion
-        if(len(game["promotions"]["promotionalOffers"]) == 0):
+        if len(game["promotions"]["promotionalOffers"]) == 0:
             continue
-        
+
         # TODO add more conditions
-        
+
         # At this point we know that the game is currently free
         # Generate a universal game object
-        free_game = { # TODO add start of promotion!!!
+        free_game = { 
             "title": game["title"],
-            "description":game["description"],
-            "free_until":game["promotions"]["promotionalOffers"][0]["promotionalOffers"][0]["endDate"]
+            "description": game["description"],
+            "free_after": game["promotions"]["promotionalOffers"][0]["promotionalOffers"][0]["startDate"],
+            "free_until": game["promotions"]["promotionalOffers"][0]["promotionalOffers"][0]["endDate"],
         }
         games.append(free_game)
     return games
-        
-        
-        
 
 
-# maybe?
-# TODO have a current promotions database, needs to be updated every time first.
-def check_or_add_to_sql(game):
+def is_new_free_game(game):
     # check if game is already in the database
     # TODO have a better check for existing game, if details change you want to update it
-    cur.execute("""SELECT * FROM current_free_games WHERE title = ?;""", (game["title"],))
-    if(len(cur.fetchall()) == 0):
+    cur.execute(
+        """SELECT * FROM free_games WHERE title = ? AND free_after = ? AND free_until = ?;""",
+        (game["title"], game["free_after"], game["free_until"]),
+    )
+    if len(cur.fetchall()) == 0:
         cur.execute(
-            """INSERT INTO current_free_games (title, description, free_until) VALUES(?,?,?);""",
+            """INSERT INTO free_games (title, description, free_after, free_until) VALUES(?,?,?,?);""",
             (
                 game["title"],
                 game["description"],
-                game["free_until"]
-            )
+                game["free_after"],
+                game["free_until"],
+            ),
         )
         con.commit()
+        return True
+    return False
 
 
+# TODO sql methods?
 
 
 # check if there are any NEW free games, return them
 def check_games():
-    free_games = get_free_games()
-
-
-# TODO removeme
-if __name__ == '__main__':
+    # Prepare SQL
     global con
     global cur
     con = sqlite3.connect("database.db")
     cur = con.cursor()
-    games = get_free_games()
-    check_or_add_to_sql(games[0])
+    # Get new Free games
+    free_games = get_free_games()
+    new_free_games = list()
+    for game in free_games:
+        if is_new_free_game(game):
+            new_free_games.append(game)
+    # Close SQL
     cur.close()
-    pass
+    return new_free_games
